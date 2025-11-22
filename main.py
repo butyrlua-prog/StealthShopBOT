9cfb9c4d59ae5d76219a9ea44b9cb397d28109ca
butyrlua-prog <butyrlua@gmail.com>
2025-11-22 16:29:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Магазин работает 🙂")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
